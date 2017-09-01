be453810c0db5dacaf369ff06cbb79beb7a0e99f
nstrucky <nickstruckmeyer@gmail.com>
2017-08-31 22:36:23 -0400

    Updated from old book - removed Line class, added GameElement

--- app/src/main/java/com/ventoray/cannongame/CannonView.java
+++ app/src/main/java/com/ventoray/cannongame/CannonView.java
@@ -21,86 +21,11 @@ public class CannonView extends SurfaceView implements SurfaceHolder.Callback {
 
     private static final String LOG_TAG = "CannonView";
 
-    private CannonThread cannonThread; //controls the game loop
-    private Activity activity;
-    private boolean dialogIsDisplayed = false;
-
-    // constants for gameplay
-    public static final int TARGET_PIECES = 7;
-    public static final int MISS_PENALTY = 2;
-    public static final int HIT_REWARD = 3;
-
-    // variables for the game loop and tracking statistics
-    private boolean gameOver;
-    private double timeLeft;
-    private int shotsFired;
-    private double totalElapsedTime;
-
-    // variables for the blocker and target
-    private Line blocker;
-    private int blockerDistance;
-    private int blockerBeginning;
-    private int blockerEnd;
-    private int intialBlockerVelocity;
-    private float blockerVelocity;
-
-    private Line target;
-    private int targetDistance;
-    private int targetBeginning;
-    private double pieceLength;
-    private int targetEnd;
-    private int initialTargetVelocity;
-    private float targetVelocity;
-
-    private int lindWidth;
-    private boolean[] hitStates;
-    private int targetPiecesHit;
-
-    //variables for the cannon and cannonball
-    private Point cannonBall;
-    private int cannonballVelocityX;
-    private int cannonballVelocityY;
-    private boolean cannonballOnscreen;
-    private int cannonballRadius;
-    private int cannonballSpeed;
-    private int cannonBaseRadius;
-    private int cannonLength;
-    private Point barrelEnd;
-    private int screenWidth;
-    private int screenHeight;
-
-    //constants and variables for managing sounds
-    private static final int TARGET_SOUND_ID = 0;
-    private static final int CANNON_SOUND_ID = 1;
-    private static final int BLOCKER_SOUND_ID = 2;
-    private SoundPool soundPool; //plays sound effects
-    private SparseIntArray soundMap; // maps Ids to SoundPool
-
-    // Paint variables used when drawing each item on the screen
-    private Paint textPaint;
-    private Paint cannonballPaint;
-    private Paint cannonPaint;
-    private Paint blockerPaint;
-    private Paint targetPaint;
-    private Paint backgroundPaint;
+    private SoundPool soundPool;
 
     public CannonView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        activity = (Activity) context;
-
-        //register SurfaceHolder.Callback listener
-        getHolder().addCallback(this);
-
-        // initialize Lines and Point representing game items;
-        blocker = new Line();
-        target = new Line();
-        cannonBall = new Point();
-
-        // initialize hitStates as a boolean array
-        hitStates = new boolean[TARGET_PIECES];
-
-        initializeSoundPool();
 
 
     }
@@ -129,6 +54,10 @@ public class CannonView extends SurfaceView implements SurfaceHolder.Callback {
         // TODO: 8/31/2017 load sounds 
 
     }
+
+
+
+
 
 
 
---
+++ app/src/main/java/com/ventoray/cannongame/elements/GameElement.java
@@ -0,0 +1,54 @@
+package com.ventoray.cannongame.elements;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+
+import com.ventoray.cannongame.CannonView;
+
+/**
+ * Created by Nick on 8/31/2017.
+ */
+
+public class GameElement {
+
+    protected CannonView view; //the view that contains this element
+    protected Paint paint = new Paint(); // Paint to draw this game element
+    protected Rect shape; // the GameElement's rectangular bounds
+    private float velocityY; // vertical velocity of this game element;
+    private int soundId;
+
+
+    public GameElement(CannonView view, int color, int soundId, int x, int y,
+                       int width, int length, float velocityY) {
+
+        this.view = view;
+        paint.setColor(color);
+        shape = new Rect(x, y, x + width, y + length);
+        this.soundId = soundId;
+        this.velocityY = velocityY;
+    }
+
+
+    // update GameElement position and check for wall collisions
+    public void update(double interval) {
+        //update vertical position
+        shape.offset(0, (int) (velocityY * interval));
+
+        //if this GameElement collides with the wall, reverse direction
+        if (shape.top < 0 && velocityY < 0 || shape.bottom > view.getScreenHeight() &&
+                velocityY > 0) {
+            velocityY *= -1;
+        }
+    }
+
+    public void draw(Canvas canvas) {
+        canvas.drawRect(shape, paint);
+    }
+
+    public void playSound() {
+        view.playSoundEffect(soundId);
+    }
+
+
+}
